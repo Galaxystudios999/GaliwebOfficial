f8306fcbb808c7fcda9bd025bdb5878da987a7e2
Galaxystudios999 <galaxystar999x@gmail.com>
2026-06-20 17:49:37 -0400

    Add files via upload

--- keyframes.js
+++ keyframes.js
@@ -1,9 +1,11 @@
 let GaliwebText = document.getElementById("GaliwebText");
+let SplashScreen = document.getElementById("SplashScreen");
 
 document.addEventListener("DOMContentLoaded", () => {
     GaliwebText.classList.add("Intro");
 
     setTimeout( () => {
         GaliwebText.style.visibility = "hidden";
+        SplashScreen.style.display = "none";
     }, 2500);
 });
